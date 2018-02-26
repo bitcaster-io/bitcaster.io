2d1bc740e6dd74bcfd7f66cb106188a469fa12fe
GigiusB <g.bronzini@gmail.com>
2018-02-26 16:32:30 +0000

    First release

---
+++ fabfile.py
@@ -0,0 +1,42 @@
+from fabric.api import *
+import fabric.contrib.project as project
+import os
+import sys
+import SimpleHTTPServer
+import SocketServer
+
+# Local path configuration (can be absolute or relative to fabfile)
+env.deploy_path = 'public'
+DEPLOY_PATH = env.deploy_path
+
+def clean():
+    if os.path.isdir(DEPLOY_PATH):
+        local('rm -rf {deploy_path}'.format(**env))
+        local('mkdir {deploy_path}'.format(**env))
+
+def build():
+    local('hugo')
+
+def rebuild():
+    clean()
+    build()
+
+def serve():
+    os.chdir(env.deploy_path)
+
+    PORT = 8000
+    class AddressReuseTCPServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    server = AddressReuseTCPServer(('', PORT), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+    server.serve_forever()
+
+def reserve():
+    build()
+    serve()
+
+def publish():
+    rebuild()
+    local('./deploy.sh --config-file deploy.conf --message "rebuilding site "')
---
+++ make_site.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
+SITEDIR=${BASEDIR}/bc-site
+
+mv $BASEDIR/docs $BASEDIR/docs_obsolete 
+REVERT=$?
+
+cd $SITEDIR
+hugo -d ../docs
+if [ $? -eq 0 ]; then
+  rm -rf ../docs_obsolete
+  mv ../docs/index.html ../docs/demo.html
+  cp content/demo.html ../docs/index.html
+else
+  echo "failed to create site"
+  if [ $REVERT != 0 ];then
+    mv ../docs_obsolete ../docs
+  fi
+fi
+
+cd $BASEDIR
